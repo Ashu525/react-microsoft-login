02fd20bead298c0d5d655193eef8e312ce52f573
Ashu525 <ashutoshshrivastava692@gmail.com>
2024-02-13 19:05:58 +0530

    feature-microsoft-login

--- src/App.js
+++ src/App.js
@@ -1,25 +1,81 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
-function App() {
+import { PageLayout } from "./components/PageLayout";
+import { loginRequest } from "./authConfig";
+import { callMsGraph } from "./graph";
+import { ProfileData } from "./components/ProfileData";
+
+import {
+  AuthenticatedTemplate,
+  UnauthenticatedTemplate,
+  useMsal,
+} from "@azure/msal-react";
+
+import "./App.css";
+
+import Button from "react-bootstrap/Button";
+
+/**
+ * Renders information about the signed-in user or a button to retrieve data about the user
+ */
+const ProfileContent = () => {
+  const { instance, accounts } = useMsal();
+  const [graphData, setGraphData] = useState(null);
+
+  function RequestProfileData() {
+    // Silently acquires an access token which is then attached to a request for MS Graph data
+    instance
+      .acquireTokenSilent({
+        ...loginRequest,
+        account: accounts[0],
+      })
+      .then((response) => {
+        callMsGraph(response.accessToken).then((response) =>
+          setGraphData(response)
+        );
+      });
+  }
+
+  return (
+    <>
+      <h5 className="card-title">Welcome {accounts[0].name}</h5>
+      <br />
+      {graphData ? (
+        <ProfileData graphData={graphData} />
+      ) : (
+        <Button variant="secondary" onClick={RequestProfileData}>
+          Request Profile Information
+        </Button>
+      )}
+    </>
+  );
+};
+
+/**
+ * If a user is authenticated the ProfileContent component above is rendered. Otherwise a message indicating a user is not authenticated is rendered.
+ */
+const MainContent = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <AuthenticatedTemplate>
+        <ProfileContent />
+      </AuthenticatedTemplate>
+
+      <UnauthenticatedTemplate>
+        <h5>
+          <center>Please sign-in to see your profile information.</center>
+        </h5>
+      </UnauthenticatedTemplate>
     </div>
   );
-}
+};
 
-export default App;
+export default function App() {
+  return (
+    <PageLayout>
+      <center>
+        <MainContent />
+      </center>
+    </PageLayout>
+  );
+}
